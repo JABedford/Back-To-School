8eaa06245a5c40afe9d3fa624357f368316c2001
James Bedford <32302360+JABedford@users.noreply.github.com>
2017-11-16 11:44:16 +0000

    Update WhatDoIWear.js

--- WhatDoIWear.js
+++ WhatDoIWear.js
@@ -9,22 +9,22 @@ var shirtLength = 30;
 var shirtSleeve = 8.71;
 
 
-if( (shirtWidth >= 18 && shirtWidth <20) && (shirtLength>=28 && shirtLength<29) && (shirtSleeve>= 8.13 && shirtSleeve<8.38  ) ){
+if( (shirtWidth >= 18 && shirtWidth <20) && (shirtLength>=28 && shirtLength<29) && (shirtSleeve>= 8.13 && shirtSleeve<8.38)) {
         console.log("S"); //Smaller size
 }
-else if ( (shirtWidth >= 20 && shirtWidth <22) && (shirtLength>=29 && shirtLength<30) && (shirtSleeve>= 8.38 && shirtSleeve<8.63  ) ){
+else if ( (shirtWidth >= 20 && shirtWidth <22) && (shirtLength>=29 && shirtLength<30) && (shirtSleeve>= 8.38 && shirtSleeve<8.63)) {
         console.log("M"); //medium size
 }
-else if ( (shirtWidth >= 22 && shirtWidth <24) && (shirtLength>=30 && shirtLength<31) && (shirtSleeve>= 8.63 && shirtSleeve<8.88  ) ){
+else if ( (shirtWidth >= 22 && shirtWidth <24) && (shirtLength>=30 && shirtLength<31) && (shirtSleeve>= 8.63 && shirtSleeve<8.88)) {
         console.log("L"); //Larger size
 }
-else if ( (shirtWidth >= 24 && shirtWidth <26) && (shirtLength>=31 && shirtLength<33) && (shirtSleeve>= 8.88 && shirtSleeve<9.63  ) ){
+else if ( (shirtWidth >= 24 && shirtWidth <26) && (shirtLength>=31 && shirtLength<33) && (shirtSleeve>= 8.88 && shirtSleeve<9.63)) {
        console.log("XL"); //Larger size
 }
-else if ( (shirtWidth >= 26 && shirtWidth <28) && (shirtLength>=33 && shirtLength<34)  && (shirtSleeve>=9.63 && shirtSleeve<10.13  ) ){
+else if ( (shirtWidth >= 26 && shirtWidth <28) && (shirtLength>=33 && shirtLength<34)  && (shirtSleeve>=9.63 && shirtSleeve<10.13)) {
        console.log("2XL"); //xtra Larger size
 }
-else if ( shirtWidth >= 28 && shirtLength>=34  && shirtSleeve>= 10.13 ){
+else if ( shirtWidth >= 28 && shirtLength>=34  && shirtSleeve>= 10.13) {
        console.log("3XL"); //xtra xtra Larger size
 }
 else{
