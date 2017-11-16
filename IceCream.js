53dfb8e5f23b5e2d417c5535dbfe3f37ec9e8de1
James Bedford <32302360+JABedford@users.noreply.github.com>
2017-11-16 13:42:39 +0000

    Update IceCream.js

--- IceCream.js
+++ IceCream.js
@@ -18,7 +18,7 @@ var flavour = "vanilla";
 var vessel = "cone";
 var toppings = "sprinkles";
 
-if ((flavour === "vanilla" || flavor === "chocolate") &&
+if ((flavour === "vanilla" || flavour === "chocolate") &&
     (vessel === "cone" || vessel === "bowl") &&
     (toppings === "sprinkles" || toppings === "peanuts")) {
         console.log(`I'd like two scoops of ${flavour} ice cream ` +
