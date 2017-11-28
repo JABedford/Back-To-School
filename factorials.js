885c1fa9113e0ec24b03a12d2312600e0ef7f3ee
James Bedford - Countryside Coder <32302360+JABedford@users.noreply.github.com>
2017-11-28 07:18:26 +0000

    Create factorials.js

---
+++ factorials.js
@@ -0,0 +1,11 @@
+/*
+ * Programming Quiz: Factorials (4-7)
+ * Write a for loop that prints out the factorial of 12.
+ */
+
+
+var solution = 1;
+for (var i = 1; i <= 12; i++) {
+    solution *= i;
+}
+console.log(solution);
