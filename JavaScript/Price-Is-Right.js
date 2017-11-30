1590e86ffa0cf77992652eedaf8ddcee60a16ad1
James Bedford - Countryside Coder <32302360+JABedford@users.noreply.github.com>
2017-11-30 13:30:50 +0000

    Create Price-Is-Right.js

---
+++ JavaScript/Price-Is-Right.js
@@ -0,0 +1,15 @@
+/*
+ * Programming Quiz: The Price is Right (6-3)
+ */
+
+// Create an array called prices and then create 8 values within the array.
+// Then change the first, third and seventh element and console.log the updated array.
+
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+prices[0] = 23.45;
+prices[2] = 34.67;
+prices[6] = 90.11;
+
+console.log(prices);
