8a4b7567c508d621ad1f9ed4a9dc02ac8d5823cc
James Bedford <32302360+JABedford@users.noreply.github.com>
2017-12-07 08:06:38 +0000

    Update AnotherTypeOfLoop.js

--- JavaScript/AnotherTypeOfLoop.js
+++ JavaScript/AnotherTypeOfLoop.js
@@ -1,11 +1,8 @@
- * Programming Quiz: Another Type of Loop (6-8)
+ /* Programming Quiz: Another Type of Loop (6-8)
  *
  * Use the existing `test` variable and write a `forEach` loop
  * that adds 100 to each number that is divisible by 3.
  *
- * Things to note:
- *  - you must use an `if` statement to verify code is divisible by 3
- *  - you can use `console.log` to verify the `test` variable when youre finished looping
  */
 
 var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4,
