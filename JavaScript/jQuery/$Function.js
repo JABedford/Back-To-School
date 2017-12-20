6ae3a7fac512fc457b22b427e4bf9c90c4d9431d
James Bedford <32302360+JABedford@users.noreply.github.com>
2017-12-20 08:04:54 +0000

    Create $Function.js

---
+++ JavaScript/jQuery/$Function.js
@@ -0,0 +1,11 @@
+/*
+For this quiz, can you use this script, which is linked in the of index.html, to change the boring placeholder image to a picture of a cute animal?
+
+Remember, you'll need to pass a function into the jQuery object to run when the document is ready.
+*/
+
+
+
+$(function() {
+  $('img').attr('src', 'http://placepuppy.it/350/150');
+});
