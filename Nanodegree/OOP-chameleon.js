83553de00e7243cfa54531ea04297008131fce39
James Bedford <32302360+JABedford@users.noreply.github.com>
2018-04-04 11:04:20 +0100

    Create OOP-chameleon.js

---
+++ Nanodegree/OOP-chameleon.js
@@ -0,0 +1,24 @@
+/*
+
+Create an object called `chameleon` with two properties:
+
+1. `color`, whose value is initially set to 'green' or 'pink'
+2. `changeColor`, a function which changes `chameleon`'s `color` to 'pink'
+    if it is 'green', or to 'green' if it is 'pink'
+
+*/
+
+
+const chameleon = {
+  color: "green",
+  changeColor: function () {
+      if (this.color === "green"){
+         chameleon.color = "pink";
+      } else {
+          chameleon.color = "green";
+      }
+    }
+};
+
+chameleon.changeColor();
+console.log(chameleon.color);
