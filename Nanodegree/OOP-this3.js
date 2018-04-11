e8ef5b596733f6dc4d1dd6ad5e7f84c71884becc
James Bedford <32302360+JABedford@users.noreply.github.com>
2018-04-11 08:19:33 +0100

    Create OOP-this3.js

---
+++ Nanodegree/OOP-this3.js
@@ -0,0 +1,12 @@
+// Consider the following:
+
+function myFunction() {
+  console.log("What is the value of 'this'?");
+}
+
+myFunction();
+// ???
+
+// When myFunction(); is executed, what is the value of this?
+
+// Window
